a6961274e13ddbcf78255e029a279bc869a0e08e
Chris Chavez <chrischavezdataengineer@gmail.com>
2020-03-28 22:53:45 -0400

    a little house keeping

--- balckjack.py
+++ balckjack.py
@@ -1,6 +1,13 @@
 import random
 import os
 
+# Blackjack game 
+# Compare the sums of the cards between D v P  
+# If P card sum is greater than 21 = BUST
+# If P card sum is less than 21 = Option Hit or Stay
+# If P option Stay compare sum of D v P 
+# If P sum < 21 && > D sum then P wins!
+# If P sum < D sum then P loses
 
 def calc_hand(hand):
 	sum = 0
