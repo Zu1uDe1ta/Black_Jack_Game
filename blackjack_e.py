8467208ea8e3332d1a7bfef2d3f179c2fc0dce9f
Chris Chavez <chrischavezdataengineer@gmail.com>
2020-03-29 14:20:08 -0400

    some more ideas to make the game unique, suites

--- blackjack_e.py
+++ blackjack_e.py
@@ -1,60 +1,12 @@
+# BlackJack or 21 game
 import random
-import os
+# THE PLANNING PHASE
+cards = []
 
-# Blackjack game 
-# Compare the sums of the cards between D v P  
-# If P card sum is greater than 21 = BUST
-# If P card sum is less than 21 = Option Hit or Stay
-# If P option Stay compare sum of D v P 
-# If P sum < 21 && > D sum then P wins!
-# If P sum < D sum then P loses
-
-
-
-
-
-dealer = []
-you = []
-player2 = []
-
-
-class Card(object):
-    """Represents an individual playing card"""
-
-    def __init__(self, rank, suit):
-        assert rank in CARD_RANK
-        self.rank = rank
-        assert suit in CARD_SUIT
-        self.suit = suit
-
-    def __repr__(self):
-        return "{:>2}{}".format(self.rank, self.suit)
-
-    def value(self):
-        """Computes the value of a card according to Blackjack rules"""
-        if self.ace():
-            value = 11
-        else:
-            try:
-                value = int(self.rank)
-            except ValueError:
-                value = 10
-        return value
-
-    def ace(self):
-        """Is this card an ace?"""
-        return self.rank == "A"
-
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
+ # Dealer cards
+dealer_cards = []
+ # Player cards
+player_cards = []
 # Deal the cards
 # Display the cards
 # Dealer Cards
@@ -95,3 +47,9 @@ if sum(player_cards) > 21:
 elif sum(player_cards) == 21:
     print("You have BLACKJACK! You Win!! 21")
 
+ # Compare the sums of the cards between D v P  
+ # If P card sum is greater than 21 = BUST
+ # If P card sum is less than 21 = Option Hit or Stay
+ # If P option Stay compare sum of D v P 
+ # If P sum < 21 && > D sum then P wins!
+ # If P sum < D sum then P loses
